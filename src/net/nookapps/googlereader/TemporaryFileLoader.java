184ba8abfecaa5b455dd2595740c87caec0c1fb8
zastrowm@gmail.com <zastrowm@gmail.com@dc4a6326-8ad3-c1dc-89e2-6b2d7ed62490>
2010-07-29 00:31:37 +0000

    Actual Initial Source Code Upload in time for the beta release

---
+++ src/net/nookapps/googlereader/TemporaryFileLoader.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * 	Filename:	TemporaryFileLoader.java
+ * 	Author:		Mackenzie Zastrow
+ * 	Use:		nookReader
+ * 	Date:		Jul 28, 2010
+ ********************************************************************************/
+package net.nookapps.googlereader;
+
+/**
+ * @author zastrowm
+ *
+ */
+public class TemporaryFileLoader {
+	public static String[] getInformation(){
+		
+		
+		return null;
+	}
+}
---
+++ src/net/nookapps/googlereader/network/WifiNotifier.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * 	Filename:	WifiNotifier.java
+ * 	Author:		Mackenzie Zastrow
+ * 	Use:		nookReader
+ * 	Date:		Jul 28, 2010
+ ********************************************************************************/
+package net.nookapps.googlereader.network;
+
+/**
+ * @author zastrowm
+ *
+ */
+public interface WifiNotifier {
+	public void onWifiEnabled();
+}
